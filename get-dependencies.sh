a4b4e28612933a3e8528a972c33362d6fb6bb60a
Link4Electronics <link4electronics@gmail.com>
2026-03-31 04:11:20 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,21 +6,33 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm     \
+    assimp                  \
+    ftgl                    \
+    python-ecdsa            \
+    python-legacy-cgi       \
+    python-setuptools-scm   \
+    python-sphinx_rtd_theme \
+    sdl2_gfx                \
+    sdl2_mixer              \
+    sdl2_ttf
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano ffmpeg-mini libdecor-mini sdl2_image-mini
 
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+make-aur-package python-pygame-sdl2
+make-aur-package python-sphinx_rtd_dark_mode
+make-aur-package uuid
 
 # If the application needs to be manually built that has to be done down here
+if [ "${DEVEL_RELEASE-}" = 1 ]; then
+	package=renpy-git
+else
+	package=renpy
+fi
+make-aur-package "$package"
+pacman -Q "$package" | awk '{print $2; exit}' > ~/version
 
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+mkdir -p ./AppDir/bin
+mv -v /usr/lib/renpy/* ./AppDir/bin
