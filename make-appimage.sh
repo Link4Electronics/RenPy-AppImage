0c1fed24b6947912b06abe8043267b3fa1a4a62c
Link4Electronics <link4electronics@gmail.com>
2026-03-31 11:19:12 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -9,13 +9,11 @@ export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/pixmaps/renpy.png
-export DESKTOP=/usr/share/applications/renpy.desktop
-export STARTUPWMCLASS=
 export DEPLOY_OPENGL=1
 export DEPLOY_SYS_PYTHON=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/renpy /usr/lib/renpy/renpy.py
+quick-sharun /usr/lib/renpy/renpy.py
 
 # Additional changes can be done in between here
 
